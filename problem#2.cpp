dc2c1ec80bc024eba91b73a344e481eec9eff6fe
Anupam <onlyprogramming000@gmail.com>
2020-06-17 14:04:13 +0530

    Problem 2

---
+++ problem#2.cpp
@@ -0,0 +1,29 @@
+/*
+This problem was asked by Uber.
+
+PROBLEM STATEMENT: Given an array of integers, return a new array such that each element at index i of the new array is the product 
+of all the numbers in the original array except the one at i.
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1],
+the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+*/
+
+//Time Complexity: 0()
+//Space Complexity: 0()
+
+#include <bits/stdc++.h>
+
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie();                        \
+    cout.tie();
+#define FILE_READ_IN freopen("input.txt", "r", stdin);
+#define FILE_READ_OUT freopen("output.txt", "w", stdout);
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
